bbf61dbd0596e96c844a3c81c9de0af1d511614a
mouakkid <58151050+mouakkid@users.noreply.github.com>
2025-08-26 11:29:05 +0100

    RevenueChart.tsx

--- components/charts/RevenueChart.tsx
+++ components/charts/RevenueChart.tsx
@@ -1,1 +1,19 @@
+'use client'
 
+import { Line } from 'react-chartjs-2'
+import {
+  Chart,
+  LineElement,
+  PointElement,
+  LinearScale,
+  CategoryScale,
+  Tooltip,
+  Legend,
+} from 'chart.js'
+
+// Enregistre les éléments requis une seule fois côté client
+Chart.register(LineElement, PointElement, LinearScale, CategoryScale, Tooltip, Legend)
+
+export default function RevenueChart({ data }: { data: any }) {
+  return <Line data={data} />
+}
